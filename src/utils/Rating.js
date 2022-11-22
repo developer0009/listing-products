5776c36c8dcdb28db9beb6125a3a92444c3a6b01
== <zaheerpersonal5@gmail.com>
2022-11-22 21:52:50 +0530

    commit

--- src/utils/Rating.js
+++ src/utils/Rating.js
@@ -53,7 +53,7 @@ const Rating = ({ rating, total, color = "#f8e825" }) => {
           <i className="fa-regular fa-star"></i>
         )}
       </span>
-      {/* <span> {total && `  of ${total} reviews`}</span> */}
+      <span className="text-dark ml-auto"> {` ${rating}`}</span>
     </div>
   );
 };
